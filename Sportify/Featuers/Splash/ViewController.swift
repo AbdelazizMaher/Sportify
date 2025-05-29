d734c037264507d85317453a59d370026f23136e
Alikotb <alikotb38@gmail.com>
2025-05-29 07:21:34 -0700

    build splash screen functionality

--- Sportify/Featuers/Splash/ViewController.swift
+++ Sportify/Featuers/Splash/ViewController.swift
@@ -7,12 +7,32 @@
 
 import UIKit
 import Kingfisher
+import Lottie
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var animation: LottieAnimationView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        var isFirst = getIsFirstTime()
+        
+//        setIsFirstTime(isFirst: true)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+       
+        animation.animationSpeed = 0.5
+        animation.play()
+        if isFirst {
+              DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+                  let vc = storyboard.instantiateViewController(withIdentifier: "onboard")
+                  self.navigationController?.pushViewController(vc, animated: true)
+              }
+          } else {
+              DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+                  let vc = storyboard.instantiateViewController(withIdentifier: "home")
+                  self.navigationController?.pushViewController(vc, animated: true)
+              }
+          }
+        
         // Do any additional setup after loading the view.
     }
 
