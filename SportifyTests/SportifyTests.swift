2ea072bc781d83e5c658f1c6ee4bc0feab57d980
AbdelazizMaher <abdelazizmaher17499@gmail.com>
2025-06-02 18:36:12 +0300

    add reachability and tests in pod file

--- SportifyTests/SportifyTests.swift
+++ SportifyTests/SportifyTests.swift
@@ -9,9 +9,10 @@ import XCTest
 @testable import Sportify
 
 final class SportifyTests: XCTestCase {
-
+    var service : NetworkServiceCM!
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        service = NetworkServiceCM()
     }
 
     override func tearDownWithError() throws {
@@ -32,5 +33,17 @@ final class SportifyTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testGetFootballData(){
+            let exp = expectation(description: "waiting for Api")
+            service.fetchDataFromApi(sport: "football", met: "Leagues") { (result: [League]?) in
+
+                XCTAssert(result?.count == 943)
+                exp.fulfill()
+
+            }
+            waitForExpectations(timeout: 5)
+
+        }
 
 }
